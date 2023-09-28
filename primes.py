40f00ebc0b92b26fc97b3c073365cfc69f877d41
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2023-09-28 18:30:24 +0100

    Completed Code

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    current = 1
+    while len(list) < number_of_primes:
+        if testIfPrime(current):
+            list.append(current)
+        current+=1
     return list
+
+def testIfPrime(number):
+    factors = 0
+    for i in range(1,number+1):
+        if number % i == 0:
+            factors += 1
+    return factors == 2
